494dc700a2832b4ec9c061776e4dec455a111953
unknown <mrrsaki0403@gmail.com>
2026-06-04 14:26:06 +0700

    header

--- src/main/java/co/istad/sengkim/elearninga01m1/features/category/CategoryController.java
+++ src/main/java/co/istad/sengkim/elearninga01m1/features/category/CategoryController.java
@@ -6,6 +6,7 @@ import co.istad.sengkim.elearninga01m1.features.category.dto.CategoryResponse;
 import co.istad.sengkim.elearninga01m1.features.category.dto.UpdateCategoryRequest;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,6 +17,7 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public Page<CategoryResponse> getAllCategories(
         @RequestParam(required = false,defaultValue ="0") int pageNumber,
@@ -24,16 +26,19 @@ public class CategoryController {
         return categoryService.allCategories(pageNumber,pageSize);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public CategoryResponse addCategory(@Valid @RequestBody CreateCategoryRequest createCategoryRequest){
         return categoryService.addCategory(createCategoryRequest);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteCategory(@PathVariable Integer id){
         categoryService.deleteCategory(id);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/{id}")
     public CategoryResponse updateCategory(@PathVariable Integer id,@Valid @RequestBody UpdateCategoryRequest updateCategoryRequest){
         return categoryService.updateCategoryById(id,updateCategoryRequest);
